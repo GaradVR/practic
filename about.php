5e9a43fb32b578d6d8e9776b95caf105ba6ccc36
GaradVR <90440360+GaradVR@users.noreply.github.com>
2022-06-28 17:23:51 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'О нас';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Наша кампания уже более пяти лет специализирована на продаже канцтоваров и школьных принадлежностей.
+        У нас вы сможите найти ручки, карандаши, тетради, клей, ножницы и многое другое.
+    </p>
+</div>
